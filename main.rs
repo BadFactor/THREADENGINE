33e99a6d59fe455f676c45ebc5ba850e7cb71e15
BadFactor <BadFactor@users.noreply.github.com>
2017-10-18 19:34:34 +0200

    update
    update

---
+++ main.rs
@@ -0,0 +1,10 @@
+// This is a comment, and will be ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+
+extern crate conrad;
+extern crate mio;
+
+fn main() {
+    println!("Hello World!");
+}
