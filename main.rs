5c0a492c6c79be9b547145c43411676799fd1eae
BadFactor <BadFactor@users.noreply.github.com>
2017-11-26 04:52:43 +0100

    update

--- main.rs
+++ main.rs
@@ -2,6 +2,21 @@
 // You can test this code by clicking the "Run" button over there ->
 // or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
 
+extern crate conrad:
+extern crate gluon;
+extern crate nalgebra;
+extern crate nphysics;
+extern crate num_cpus;
+extern crate openvr-rs;
+extern crate rust-raytracer;
+extern crate specs;
+extern crate vulkano;
+
+// count logical cores this process could try to use
+let num = num_cpus::get();
+
+use std::io;
+
 extern crate conrad;
 extern crate mio;
 
